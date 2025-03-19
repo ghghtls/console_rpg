d01690213da41eff8c21c9680e224115986c1a2e
ghghtls <ghghtls2@gmail.com>
2025-03-19 20:24:53 +0900

    feat:몬스터 랜덤기능 추가가

--- bin/console_rpg.dart
+++ bin/console_rpg.dart
@@ -2,6 +2,7 @@ import 'package:console_rpg/character.dart';
 import 'package:console_rpg/game.dart';
 import 'package:console_rpg/monster.dart';
 import 'package:console_rpg/utils.dart';
+import 'dart:math';
 
 void main() {
   Game game = Game();
@@ -22,12 +23,21 @@ void main() {
 
   //로드 기능 몬스터.txt 파일에 몇마리 있는지 로드 기능
   print("몬스터 ${game.monsters.length}마리 로드 완료");
-  print("새로운 몬스터가 나타났습니다");
-  for (int i = 0; i < game.monsters.length; i++) {
-    print(
-      '${game.monsters[i].monsterName} - 체력: ${game.monsters[i].monsterHp},공격력:${game.monsters[i].max}',
-    );
-  }
+  // 랜덤 인덱스 뽑기
+  Random random = Random();
+  int randomIndex = random.nextInt(game.monsters.length);
+
+  // 랜덤 몬스터 출력
+  var randomMonster = game.monsters[randomIndex];
+  print(
+    '${randomMonster.monsterName} - 체력: ${randomMonster.monsterHp}, 공격력: ${randomMonster.max}',
+  );
+  // print("새로운 몬스터가 나타났습니다");
+  // for (int i = 0; i < game.monsters.length; i++) {
+  //   print(
+  //     '${game.monsters[i].monsterName} - 체력: ${game.monsters[i].monsterHp},공격력:${game.monsters[i].max}',
+  //   );
+  // }
 
   bool isWin = true; // 또는 false로 패배 시뮬레이션
 
