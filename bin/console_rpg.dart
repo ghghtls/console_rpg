515c30157d3f70f552031afc6ea028df4ecc3860
ghghtls <ghghtls2@gmail.com>
2025-03-20 11:12:07 +0900

    feat:결과값 추가기능능

--- bin/console_rpg.dart
+++ bin/console_rpg.dart
@@ -51,14 +51,8 @@ void main() {
   print(
     '${randomMonster.monsterName} - 체력: ${randomMonster.monsterHp}, 공격력: ${randomMonster.max}',
   );
-  /* print("새로운 몬스터가 나타났습니다");
-   for (int i = 0; i < game.monsters.length; i++) {
-     print(
-      '${game.monsters[i].monsterName} - 체력: ${game.monsters[i].monsterHp},공격력:${game.monsters[i].max}',
-     );
- }*/
-  // 보너스 체력 지급 시도
 
+  // 보너스 체력 지급 시도
   int bonus = game.bonusHealth(game.character);
   bool isWin = true; // 전역으로 먼저 선언
   bool itemActive = false; //  이번 턴에만 발동할지 체크
@@ -164,37 +158,4 @@ void main() {
       break;
     }
   }
-
-  /*while (true) {
-    print('$characterName의 턴');
-    print('행동을 선택하세요(1: 공격, 2: 방어):');
-    String? number = stdin.readLineSync();
-    if (number == '1') {
-      print(
-        '$characterName이(가) ${randomMonster.monsterName}에게  ${randomMonster.max}의 데미지를 입혔습니다',
-      );
-    } else if (number == '2') {
-      print('$characterName이(가) 방어 태세를 취하여 $bonus 만큼 체력을 얻었습니다.');
-    } else {
-      print('잘못 입력 했습니다.');
-    }
-
-    print('${randomMonster.monsterName}의 턴');
-    if (randomMonster.monsterName != null) {
-      print(
-        '${randomMonster.monsterName}이(가) $characterName에게  ${randomMonster.max}의 데미지를 입혔습니다',
-      );
-    } else if (randomMonster.monsterHp == 0) {
-      print('다음 몬스터와 싸우시겠습니까? (y/n)');
-      String? answer = stdin.readLineSync();
-      if (answer == 'y' || answer == 'Y') {
-        continue;
-      } else {
-        break;
-      }
-    } else if (randomMonster.monsterName == null) {
-      print('${randomMonster.monsterName}을(를) 물리쳤습니다.');
-      break;
-    }
-  }*/
 }
